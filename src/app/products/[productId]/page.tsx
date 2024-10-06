a8abd839c82c5c5403834b563a2b35f7a4ffd049
murat.kaplan <mkaplan@luca.com.tr>
2024-10-06 16:46:02 +0300

    next

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,3 @@
+export default  function ProductDetail({params}: {params: {productId: string}}) {
+    return <h1>Product Detail {params.productId}</h1>;
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function Products() {
+    return(<>
+    <h1>Products</h1>
+    <p>Product 1</p>
+    <p>Product 2</p>
+    <p>Product 3</p>
+    </>
+    );
+}
